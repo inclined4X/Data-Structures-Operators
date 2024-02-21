f539baf0e7aa33d64e2837b7193e42e44a9f6256
inclined4X <jezemiahsam48@gmail.com>
2024-02-21 23:10:50 +0000

    destructuring in destructuring

--- script.js
+++ script.js
@@ -56,3 +56,10 @@ console.log(main, secondary);
 // Recieve two return values from a function
 const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse);
+
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
