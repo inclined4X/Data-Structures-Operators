d2c475b13330092adeded2471cba9de9f3016b65
inclined4X <jezemiahsam48@gmail.com>
2024-02-24 00:56:56 +0000

    adding more values to check short circuting

--- script.js
+++ script.js
@@ -59,6 +59,9 @@ const restaurant = {
 console.log(3 || 'Jonas');
 console.log('' || 'Jonas');
 console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
 
 /*
 // DESTRUCTURING
