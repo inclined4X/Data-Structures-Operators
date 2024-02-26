e07af5b46348be54689993bfe09b6b8b17aeb1c8
inclined4X <jezemiahsam48@gmail.com>
2024-02-26 01:23:24 +0000

    chaining sets

--- script.js
+++ script.js
@@ -54,10 +54,18 @@ const restaurant = {
 };
 
 // MAP
-const res = new Map();
-res.set('name', 'Classico Italiano');
-res.set(1, 'Firenze, Italy');
-console.log(res.set(2, 'Lisbon, Portugal'));
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('Categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
 /*
 // SETS
 const ordersSet = new Set(['Pasta', 'Pizza', 'Risotto', 'Pasta', 'Pizza']);
