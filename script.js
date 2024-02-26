775b1796a67536fcf9b48d609d22a9d616349ec5
inclined4X <jezemiahsam48@gmail.com>
2024-02-26 00:51:24 +0000

    clear set and loop set with for of loop

--- script.js
+++ script.js
@@ -64,7 +64,11 @@ console.log(ordersSet.has('Bread'));
 ordersSet.add('Garlic Bread');
 ordersSet.add('Garlic Bread');
 ordersSet.delete('Risotto');
+// ordersSet.clear();
 console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
 /* 
 // looping objects
 // PROPERTY NAMES
